d64c7891f54f0b366507566e675af86096b41eb1
BenturaPeralez <90201012+BenturaPeralez@users.noreply.github.com>
2021-09-29 13:05:47 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,10 +1,4 @@
-
-
-
-
-
-
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-//var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
+moment().format();                                // "2014-09-08T08:02:17-05:00" (ISO 8601, no fractional seconds)
+moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); // "Sunday, February 14th 2010, 3:25:50 pm"
+moment().format("ddd, hA");                       // "Sun, 3PM"
+moment().format("[Today is] dddd");               // "Today is Sunday"
