51ffd9743d1fdf8591a34c95776ae9ae169148eb
BenturaPeralez <90201012+BenturaPeralez@users.noreply.github.com>
2021-10-04 11:02:04 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 $(`#timeFormatOne`).text(moment().format("MM/DD/YYYY HH:mm:ss"));
 
 $(".saveBtn").on("click", function(){
-    console.log("clicked:",  this)
+    debugger
+    let text = $(this).siblings(".textarea").val();
+    let time =$(this).parent().attr("id")
     })
