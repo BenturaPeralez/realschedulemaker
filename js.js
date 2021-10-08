b6f9f25c866f351dd871df42656fab38ff4696c7
BenturaPeralez <90201012+BenturaPeralez@users.noreply.github.com>
2021-10-08 11:57:22 -0500

    Add files via upload

--- js.js
+++ js.js
@@ -1,28 +1,7 @@
 $(`#timeFormatOne`).text(moment().format("MM/DD/YYYY HH:mm:ss"));
 
 $(".saveBtn").on("click", function(){
-    let text = $(this).siblings(".textareacontainer").children("textarea").val();
-    let time =$(this).parent().attr("id");
-    localStorage.setItem(time, text);
+    debugger
+    let text = $(this).siblings(".textarea").val();
+    let time =$(this).parent().attr("id")
     })
-    
-    var text = localStorage.getItem("10")
-
-    
-$("#10").children(".textareacontainer").children("textarea").val(text);
-
-$("#9").children(".textareacontainer").children("textarea").val(text);
-
-$("#11").children(".textareacontainer").children("textarea").val(text);
-
-$("#12").children(".textareacontainer").children("textarea").val(text);
-
-$("#1").children(".textareacontainer").children("textarea").val(text);
-
-$("#2").children(".textareacontainer").children("textarea").val(text);
-
-$("#3").children(".textareacontainer").children("textarea").val(text);
-
-$("#4").children(".textareacontainer").children("textarea").val(text);
-
-$("5").children(".textareacontainer").children("textarea").val(text);
