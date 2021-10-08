3b90f337ba87601fa5f349238f77e7de250d6d35
Bentura Peralez <bperalez001@gmail.com>
2021-10-08 11:53:29 -0500

    resubmission

--- js.js
+++ js.js
@@ -1,7 +1,28 @@
 $(`#timeFormatOne`).text(moment().format("MM/DD/YYYY HH:mm:ss"));
 
 $(".saveBtn").on("click", function(){
-    debugger
-    let text = $(this).siblings(".textarea").val();
-    let time =$(this).parent().attr("id")
+    let text = $(this).siblings(".textareacontainer").children("textarea").val();
+    let time =$(this).parent().attr("id");
+    localStorage.setItem(time, text);
     })
+    
+    var text = localStorage.getItem("10")
+
+    
+$("#10").children(".textareacontainer").children("textarea").val(text);
+
+$("#9").children(".textareacontainer").children("textarea").val(text);
+
+$("#11").children(".textareacontainer").children("textarea").val(text);
+
+$("#12").children(".textareacontainer").children("textarea").val(text);
+
+$("#1").children(".textareacontainer").children("textarea").val(text);
+
+$("#2").children(".textareacontainer").children("textarea").val(text);
+
+$("#3").children(".textareacontainer").children("textarea").val(text);
+
+$("#4").children(".textareacontainer").children("textarea").val(text);
+
+$("5").children(".textareacontainer").children("textarea").val(text);
